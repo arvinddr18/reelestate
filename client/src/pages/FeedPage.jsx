f85549d737f9759c0eb22f4e670b8f5df873664c
Arvin <arvindarvinddr@gmail.com>
2026-03-29 10:30:01 +0530

    Fix: Added missing useRef import to fix blank screen crash

--- client/src/pages/FeedPage.jsx
+++ client/src/pages/FeedPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { MdOutlineDoubleArrow, MdShield } from 'react-icons/md'; 
 import { IoMdSearch, IoMdAdd } from 'react-icons/io'; 

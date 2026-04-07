b0b757df6eda24f5b082f5c4463af1345b7654df
Arvin <arvindarvinddr@gmail.com>
2026-04-07 12:35:00 +0530

    chsnsges

--- client/src/components/AnimatedMessageBubble.jsx
+++ client/src/components/AnimatedMessageBubble.jsx
@@ -297,7 +297,7 @@ export default function AnimatedMessageBubble({ msg, isMe, onReply, onEdit, onDe
                 animate={{ opacity: 1 }}
                 exit={{ opacity: 0 }}
                 className="z-[99999]"
-          style={{ position: 'fixed', top: '40%', left: '50%', transform: 'translate(-50%, -50%)' }}
+         style={{ position: 'absolute', bottom: '110%', left: '50%', transform: 'translateX(-50%)' }}
           onClick={(e) => e.stopPropagation()}
               ><div className="relative">
                   

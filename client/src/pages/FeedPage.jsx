0898fa52b87431afb1c3c52ff5e9c005cb2a6704
Arvin <arvindarvinddr@gmail.com>
2026-04-10 16:33:39 +0530

    chad

--- client/src/pages/FeedPage.jsx
+++ client/src/pages/FeedPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { MdOutlineDoubleArrow, MdShield } from 'react-icons/md'; 
-import { IoMdSearch, IoMdAdd } from 'react-icons/io'; 
+import { IoMdSearch, IoMdAdd, IoMdNotifications } from 'react-icons/io';
 import importedLogo from '../assets/nodexa-logo.png'; // 👈 Your exact file!
 import PostCard from '../components/feed/PostCard'; 
 import ReelSwiper from '../components/reels/ReelSwiper';

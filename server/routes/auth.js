55fda7c5bc4077133e8151e975e7021db21e48c2
Arvin <arvindarvinddr@gmail.com>
2026-04-27 20:24:40 +0530

    djiew

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,7 +3,7 @@
  */
 const express = require('express');
 const router = express.Router();
-const { register, login, getMe, getLinkedAccounts, logoutAll, changePassword, verify2FALogin } = require('../controllers/authController');
+const { register, login, getMe, getLinkedAccounts, logoutAll, changePassword, verify2FALogin, sendPasswordOTP, resetPasswordWithOTP } = require('../controllers/authController');
 const { protect } = require('../middleware/auth'); // Assuming you have an auth middleware
 
 router.post('/verify-2fa-login', verify2FALogin);

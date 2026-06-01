d0a5519fce7b9b19653f0717cc7d1f5bbc521453
Arvin <arvindarvinddr@gmail.com>
2026-06-01 16:36:58 +0530

    vbhje

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,30 +1,23 @@
 const nodemailer = require('nodemailer');
 
 const sendEmail = async (options) => {
-  // 1. Create the transporter (The Mailtruck)
   const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',  
-    port: 587,  
-    secure: false,
-    family: 4,              
+    host: 'smtp-relay.brevo.com',
+    port: 587,
+    secure: false, // false for port 587
     auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
+      user: process.env.BREVO_USER,
+      pass: process.env.BREVO_PASS,
     },
-    tls: {
-      rejectUnauthorized: false  // ✅ Prevents TLS errors on Render
-    }
   });
 
-  // 2. Define the email options (The Letter)
   const mailOptions = {
-    from: `Nodexa Security <${process.env.EMAIL_USER}>`,
+    from: `"Nodexa Security" <ad2b94001@smtp-brevo.com>`, // Use the login email as the sender
     to: options.email,
     subject: options.subject,
     html: options.html,
   };
 
-  // 3. Send it!
   await transporter.sendMail(mailOptions);
 };
 
